b6feca9d464dddcec9575d6e2bb595a078ad4df4
prgram769 <prgram02@gmail.com>
2025-11-02 19:56:59 +0100

    avances, parte del javascript y reestructuracion de la estructura del proyecto

---
+++ js/newSheet.js
@@ -0,0 +1,30 @@
+const dateInput = document.getElementById("date");
+const descriptionInput = document.getElementById("description");
+const categoryInput = document.getElementById("category");
+const amountInput = document.getElementById("amount");
+const submitButton = document.getElementById("submitButton");
+
+function addRemoveMoney() {
+  var elementID = 0;
+
+  elementID++;
+
+  var dateInputValue = dateInput.value;
+  var descriptionInputValue = descriptionInput.value;
+  var categoryInputValue = categoryInput.value;
+  var amountInputValue = amountInput.value;
+  
+  // Create horizontal line
+
+  var horizontalLine = document.createElement("p");
+  
+  horizontalLine.textContent = " ";
+  horizontalLine.setAttribute("id", "horizontalLine");
+  horizontalLine.style.borderBottom = "2px solid black";
+
+  body.appendChild(horizontalLine);
+}
+
+submitButton.addEventListener("click", submitButton => {
+  addRemoveMoney();
+})
