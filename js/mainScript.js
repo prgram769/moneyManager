0f8f745256715e2cb713c3fcf9919350511427bb
prgram769 <prgram02@gmail.com>
2025-11-07 21:01:11 +0100

    seccion arreglada e implementacion de las tablas

--- js/mainScript.js
+++ js/mainScript.js
@@ -3,7 +3,11 @@ const descriptionInput = document.getElementById("description");
 const categoryInput = document.getElementById("category");
 const amountInput = document.getElementById("amount");
 const submitButton = document.getElementById("submitButton");
-const mainContainer = document.getElementsByClassName("mainContainer");
+
+var mainContainer = document.getElementById("mainContainerID");
+var form = document.getElementById("formID");
+var main = document.getElementById("mainSemantic");
+var elementID = 0;
 
 function addRemoveMoney() {
 
@@ -20,32 +24,69 @@ function addRemoveMoney() {
     horizontalLine.setAttribute("id", "horizontalLine");
     horizontalLine.style.borderBottom = "2px solid black";
 
-    document.body.appendChild(horizontalLine);
+    mainContainer.appendChild(horizontalLine);
   }
 
   // Create section for table 1
 
   if (document.getElementById("section") == null) {
-    var elementID = 0;
-
-    elementID++;
 
     var sectionTable1 = document.createElement("section");
 
-    sectionTable1.setAttribute("id", "section");
+    sectionTable1.setAttribute("id", `section${elementID}`);
 
-    document.body.appendChild(sectionTable1);
+    main.appendChild(sectionTable1);
 
-    if (document.getElementById("addSectionBtn")) {
+    if (document.getElementById("addSectionBtn") == null) {
       var addSectionBtn = document.createElement("button");
 
       addSectionBtn.setAttribute("id", "addSectionBtn");
+      addSectionBtn.textContent = "Add section";
 
-      console.log(addSectionBtn);
-
-      mainContainer.appendChild(addSectionBtn);
+      form.appendChild(addSectionBtn);
     }
   }
+
+  // create the table
+  
+  var table = document.createElement("table");
+
+  table.setAttribute("id", `table${elementID}`);
+
+  var tableTitle = document.createElement("caption");
+
+  tableTitle.setAttribute("id", `tableTitle${elementID}`);
+  tableTitle.textContent = `Table${elementID}`;
+
+  var tableDate = document.createElement("th");
+
+  tableDate.setAttribute("id", `tableDate${elementID}`);
+  tableDate.textContent = "Date";
+
+  var tableDescription = document.createElement("th");
+
+  tableDescription.setAttribute("id", `tableDescription${elementID}`);
+  tableDescription.textContent = "Description";
+
+  var tableCategory = document.createElement("th");
+
+  tableCategory.setAttribute("id", `tableCategory${elementID}`);
+  tableCategory.textContent = "Category";
+
+  var tableAmount = document.createElement("th");
+
+  tableAmount.setAttribute("id", `tableAmount${elementID}`);
+  tableAmount.textContent = "Amount";
+
+  sectionTable1.appendChild(table);
+
+  table.appendChild(tableTitle);
+  table.appendChild(tableDate);
+  table.appendChild(tableDescription);
+  table.appendChild(tableCategory);
+  table.appendChild(tableAmount);
+
+  elementID++;
 }
 
 submitButton.addEventListener("click", (event) => {
